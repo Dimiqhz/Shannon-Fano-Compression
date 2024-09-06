a8213918cb254e23c803767ee6c4de3abff9860f
Dimiqhz <115684884+Dimiqhz@users.noreply.github.com>
2024-09-06 12:49:27 +0300

    Added decoder

--- Shennon.py
+++ Shennon.py
@@ -14,12 +14,10 @@ def compress_data(data):
     split = splitter([i['probability'] for i in sorted_compressor], 0)
     encoder(sorted_compressor, split)
 
-    # Сохраняем результат сжатия в файл
     compressed_text = "".join(i['code'] for i in sorted_compressor)
     with open("compressed_text.txt", "w") as output_file:
         output_file.write(compressed_text)
 
-    # Сохраняем словарь в файл
     with open("dict.txt", "w", encoding="utf-8") as dict_file:
         for i in sorted_compressor:
             dict_file.write(f"Символ - {i['original']} :: Код - {i['code']}\n")
@@ -59,24 +57,48 @@ def encoder(compressor, split):
             i['code'] += '1'
 
 
-# Основная функция
+def decode_data():
+    with open("compressed_text.txt", "r") as compressed_file:
+        compressed_text = compressed_file.read()
+
+    code_dict = {}
+    with open("dict.txt", "r", encoding="utf-8") as dict_file:
+        for line in dict_file:
+            parts = line.strip().split(" :: Код - ")
+            char = parts[0].split("Символ - ")[1]
+            code = parts[1]
+            code_dict[code] = char
+
+    decoded_text = ""
+    temp_code = ""
+    for bit in compressed_text:
+        temp_code += bit
+        if temp_code in code_dict:
+            decoded_text += code_dict[temp_code]
+            temp_code = ""
+
+    return decoded_text
+
+
 if __name__ == '__main__':
-    choice = int(input("Введите 1, чтобы ввести текст вручную, или 2, чтобы загрузить из файла: "))
+    choice = int(input("Введите 1, чтобы ввести текст вручную, 2, чтобы загрузить из файла или 3, чтобы декодировать текст: "))
     if choice == 1:
         text = input("Введите текст для сжатия: ")
+        compress_data(text)
+        print("Данные сохранены в файл compressed_text.txt")
+        print("Словарь сохранен в файл dict.txt")
     elif choice == 2:
         file_path = input("Укажите путь к файлу для загрузки текста: ")
         try:
             with open(file_path, 'r') as file:
                 text = file.read()
+                compress_data(text)
+                print("Данные сохранены в файл compressed_text.txt")
+                print("Словарь сохранен в файл dict.txt")
         except FileNotFoundError:
             print(f"Файл '{file_path}' не найден.")
-            exit()
+    elif choice == 3:
+        decoded_text = decode_data()
+        print(f"Декодированный текст: {decoded_text}")
     else:
         print("Выбрана неверная опция.")
-        exit()
-
-    print(f"Исходный текст: {text}")
-    compress_data(text)
-    print("Данные сохранены в файл compressed_text.txt")
-    print("Словарь сохранен в файл dict.txt")
